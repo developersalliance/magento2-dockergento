c0682b6e9c3d6c3865c0f4468b4c2f873a6d9aa4
Dmitriy Gallyamov <dmitrijs.gallamovs@gmail.com>
2019-09-24 17:17:56 +0400

    Reverted xdebug command remove

---
+++ console/commands/debug-off.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+${TASKS_DIR}/start_service_if_not_running.sh ${SERVICE_PHP_XDEBUG}
+
+${COMMANDS_DIR}/exec-xdebug.sh sed -i -e 's/^\zend_extension/;zend_extension/g' /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
+${DOCKER_COMPOSE} restart ${SERVICE_PHP_XDEBUG}
+
+printf "> ${GREEN}xdebug disabled${COLOR_RESET}\n"
