3fca2be6d572351db1a6bf067dd0141f6cabede4
Dmitriy Gallyamov <dmitrijs.gallamovs@gmail.com>
2020-05-20 18:34:59 +0300

    Added varnish support

--- console/commands/setup.sh
+++ console/commands/setup.sh
@@ -153,7 +153,7 @@ add_custom_paths_in_file()
 {
     FILE_TO_EDIT=$1
     SUFFIX_BIND_PATH=$2
-    FILE_PATHS=(auth.json artifacts patches .gitignore)
+    FILE_PATHS=(auth.json artifacts patches .gitignore m2-hotfixes)
     BIND_PATHS=""
 
     for i in ${FILE_PATHS[@]}
